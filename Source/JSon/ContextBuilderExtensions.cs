2cd39200cc7c8a68e811064c3a512d8514344257
Brian Elgaard Bennett <bb@saxobank.com>
2017-05-22 10:04:40 +0200

    Added JSon project

---
+++ Source/JSon/ContextBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using LeanTest.Core.ExecutionHandling;
+
+namespace JSon
+{
+    /// <summary>
+    /// Adds support for adding test data in Json format.
+    /// </summary>
+    public static class ContextBuilderExtensions
+    {
+        /// <summary>
+        /// Declare data of type <c>T</c> to be stored, then used to fill in 'mocks' and 'state' during <c>Build</c>.
+        /// </summary>
+        public static ContextBuilder WithData<T>(this ContextBuilder contextBuilder, string json)
+        {
+            var creator = new JsonDataCreatorForTest<T>();
+            contextBuilder.WithData(creator.DeserializeFromJson(json));
+
+            return contextBuilder;
+        }
+        /// <summary>
+        /// Declare an enumeration of data of type <c>T</c> to be stored, then used to fill in 'mocks' and 'state' during <c>Build</c>.
+        /// </summary>
+        public static ContextBuilder WithEnumerableData<T>(this ContextBuilder contextBuilder, string json)
+        {
+            var creator = new JsonDataCreatorForTest<T>();
+            IEnumerable<T> enumerable = creator.DeserializeCollectionFromJsonArray(json);
+            contextBuilder.WithEnumerableData(enumerable);
+
+            return contextBuilder;
+        }
+    }
+}
---
+++ Source/JSon/JsonDataCreatorForTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace JSon
+{
+    /// <summary>
+    /// Utility for creating test data of type <c>T</c> from JSon.
+    /// </summary>
+    public class JsonDataCreatorForTest<T>
+    {
+        /// <summary>
+        /// Deserialize a single instance of type <c>T</c>.
+        /// </summary>
+        public T DeserializeFromJson(string jsonResource)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonResource);
+        }
+
+        /// <summary>
+        /// Interprets a string that represents an array of Json documents of type <c>T</c>.
+        /// </summary>
+        public IEnumerable<T> DeserializeCollectionFromJsonArray(string jsonResource)
+        {
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonResource);
+        }
+    }
+}
