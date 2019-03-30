4cef2ae8a78f0a2e22d909c8250ab80cbad6d662
Brian Elgaard <github@elgaard.com>
2019-03-30 18:30:46 +0100

    Fixed missing XML comments

--- Source/Core/Core/ExecutionHandling/MultiAssertForTException.cs
+++ Source/Core/Core/ExecutionHandling/MultiAssertForTException.cs
@@ -9,16 +9,10 @@ namespace LeanTest.Core.ExecutionHandling
     /// </summary>
     public static class MultiAssertForTException
     {
-        /// <summary>
-        /// Executes all actions regardless of failed assertions (a.k.a. <c>TException</c> thrown). If any assertions fail, a single 
-        /// assertion failure with aggregated assertion message texts is generated (i.e. a <c>AggregatedMessagesException</c> is thrown).
-        /// </summary>
-        /// <param name="actions">
-        /// A list of actions that represent a number of Assert method invocations (Assert.IsTrue etc.)
-        /// </param>
-        /// <remarks>
-        /// It is still Best Practice to have a single assert per unit test in most cases (but more often than not, developer tests will have several asserts per test).
-        /// Refer to the following blog posts for proper usage of MultiAssert,
+        /// <summary>Executes all actions regardless of failed assertions (a.k.a. <c>TException</c> thrown). If any assertions fail, a single 
+        /// assertion failure with aggregated assertion message texts is generated (i.e. a <c>AggregatedMessagesException</c> is thrown).</summary>
+        /// <param name="actions">A list of actions that represent a number of Assert method invocations (Assert.IsTrue etc.)</param>
+        /// <remarks>Refer to the following blog posts for proper usage of MultiAssert,
         ///  - https://elgaard.blog/2011/02/06/multiple-asserts-in-a-single-unit-test-method/
         ///  - https://elgaard.blog/2013/05/26/even-more-asserts-in-a-single-unit-test-method/
         /// </remarks>
--- Source/MSTest/ExceptionAssert.cs
+++ Source/MSTest/ExceptionAssert.cs
@@ -5,13 +5,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeanTest.MSTest
 {
-	/// <summary>
-	/// The MS Test adaption to <c>ExceptionAssertTException</c>.
-	/// </summary>
+	/// <summary>The MS Test adaption to <c>ExceptionAssertTException</c>.</summary>
 	public static class ExceptionAssert
 	{
+		/// <summary>Throws an <c>AssertFailedException</c> exception unless <c>func</c> throws a <c>TException</c>.</summary>
 		public static TException Throws<TException>(Func<Task> func, string message = "") where TException : Exception =>
 			ExceptionAssertTException.Adapter(func, message, ExceptionAssertTException.Throws<TException>, m => new AssertFailedException(m));
+		/// <summary>Throws an <c>AssertFailedException</c> exception unless <c>action</c> throws a <c>TException</c>.</summary>
 		public static TException Throws<TException>(Action action, string message = "") where TException : Exception =>
 			ExceptionAssertTException.Adapter(action, message, ExceptionAssertTException.Throws<TException>, m => new AssertFailedException(m));
 	}
