832b93693b05192c2cfa947982bf2a86864ce27b
Brian Elgaard Bennett <bb@saxobank.com>
2019-04-29 13:06:43 +0200

    Removed R# detected warnings

--- Source/MSTest/DescriptionAttributeFix.cs
+++ Source/MSTest/DescriptionAttributeFix.cs
@@ -7,7 +7,5 @@
 		public const string Prefix = @"Description = ###---";
 		/// <summary>A postfix put in a .trx-file after each tag value.</summary>
 		public const string Postfix = @"---###";
-		/// <summary>Put in a .trx-file for each 'not implemented' tag value.</summary>
-		public const string NotImplemented = @"LeanTest.NotImplemented";
 	}
 }
