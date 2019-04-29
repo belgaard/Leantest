13a3d6452c58dc6a06c5b8c3fd28faf59fcfbfe2
Brian Elgaard Bennett <bb@saxobank.com>
2019-04-29 13:02:40 +0200

    Got rid of all warnings for now

--- Source/MSTest/DescriptionAttributeFix.cs
+++ Source/MSTest/DescriptionAttributeFix.cs
@@ -1,5 +1,6 @@
 ﻿namespace LeanTest.MSTest
 {
+	/// <summary>Pre- and post-fixes for information put in .trx-files.</summary>
 	public static class DescriptionAttributeFix
 	{
 		/// <summary>A prefix put in a .trx-file before each tag value.</summary>
