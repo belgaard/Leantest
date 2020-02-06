e57f92659df24b485258d966089fc4067c770641
Brian Elgaard Bennett <bb@saxobank.com>
2020-02-06 13:13:55 +0100

    Obsolete MockingBuilderFactory

--- Source/Mock/MockingBuilderFactory.cs
+++ Source/Mock/MockingBuilderFactory.cs
@@ -1,13 +1,17 @@
-﻿namespace LeanTest.Mock
+﻿using System;
+
+namespace LeanTest.Mock
 {
     /// <summary>
     /// Instantiate and initialize a mocking builder to handle IMockForData-based mocks
     /// </summary>
+    [Obsolete]
     public static class MockingBuilderFactory
     {
         /// <summary>
         /// This method is here for backwards compatibility - you can safely call it or not, it will do nothing at all.
         /// </summary>
+        [Obsolete("This method is here for backwards compatibility - you can safely call it or not, it will do nothing at all.")]
         public static void Initialize() {}
     }
 }
