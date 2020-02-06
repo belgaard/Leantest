d9b7632c834b73f07847b6c58eebbdb9d81a4103
Brian Elgaard Bennett <bb@saxobank.com>
2020-02-06 16:25:14 +0100

    Warnings

--- Source/MSTest/ExceptionAssert.cs
+++ Source/MSTest/ExceptionAssert.cs
@@ -7,6 +7,7 @@ namespace LeanTest.MSTest
 	/// <summary>The MS Test adaption to <c>ExceptionAssertTException</c>.</summary>
 	public static class ExceptionAssert
 	{
+		/// <summary>Throws an <c>AssertFailedException</c> exception if <c>action</c> throws an exception.</summary>
 		public static void DoesNotThrow(Action action, string message = "") =>
 			ExceptionAssertTException.Adapter(action, message, ExceptionAssertTException.DoesNotThrow, m => new AssertFailedException(m));
 	}
