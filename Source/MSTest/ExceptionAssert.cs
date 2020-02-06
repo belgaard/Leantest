5c817ca10d0f3a345f279b368abb12a1e2771e6a
Brian Elgaard Bennett <bb@saxobank.com>
2020-02-06 16:09:55 +0100

    Added DoesNotThrow

--- Source/MSTest/ExceptionAssert.cs
+++ Source/MSTest/ExceptionAssert.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using LeanTest.Core.ExecutionHandling;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,13 +7,7 @@ namespace LeanTest.MSTest
 	/// <summary>The MS Test adaption to <c>ExceptionAssertTException</c>.</summary>
 	public static class ExceptionAssert
 	{
-		/// <summary>Throws an <c>AssertFailedException</c> exception unless <c>func</c> throws a <c>TException</c>.</summary>
-		[Obsolete("Use the MsTest Assert.ThrowsException")]
-		public static TException Throws<TException>(Func<Task> func, string message = "") where TException : Exception =>
-			ExceptionAssertTException.Adapter(func, message, ExceptionAssertTException.Throws<TException>, m => new AssertFailedException(m));
-		/// <summary>Throws an <c>AssertFailedException</c> exception unless <c>action</c> throws a <c>TException</c>.</summary>
-		[Obsolete("Use the MsTest Assert.ThrowsException")]
-		public static TException Throws<TException>(Action action, string message = "") where TException : Exception =>
-			ExceptionAssertTException.Adapter(action, message, ExceptionAssertTException.Throws<TException>, m => new AssertFailedException(m));
+		public static void DoesNotThrow(Action action, string message = "") =>
+			ExceptionAssertTException.Adapter(action, message, ExceptionAssertTException.DoesNotThrow, m => new AssertFailedException(m));
 	}
 }
