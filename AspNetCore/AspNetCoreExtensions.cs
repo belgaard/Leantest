3535c554db03aaf013e83cfaf6bd5672de6966f5
Brian Elgaard Bennett <bb@saxobank.com>
2020-03-03 13:05:28 +0100

    Added the AspNetCore project

---
+++ AspNetCore/AspNetCoreExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using LeanTest.Core.ExecutionHandling;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace LeanTest
+{
+	/// <summary>This is used to integrate the .Net Core web host builder pattern with the Lean Test context builder pattern.</summary>
+	public static class AspNetCoreContextBuilderFactory
+	{
+		private static readonly Dictionary<Type, object> Extras = new Dictionary<Type, object>();
+
+		/// <summary>ASP.NET Core version of setting up IoC and builders.</summary>
+		/// <param name="mode">Always use ReCreate to recreate the IoC container before each test.</param>
+		/// <param name="webHostBuilder">The ASP.NET Core web host builder from production code, overridden with mocks using .ConfigureTestServices().</param>
+		/// <param name="iocContainerFactory">Creates the IoC container, such as e.g. 'provider => new IocContainer(provider)'.</param>
+		public static void Initialize(CleanContextMode mode, Func<IWebHostBuilder> webHostBuilder, Func<IServiceProvider, IIocContainer> iocContainerFactory) =>
+			ContextBuilderFactory.Initialize(mode, () =>
+			{
+				foreach (var extra in Extras.Values) (extra as IDisposable)?.Dispose();
+				Extras.Clear();
+
+				var server = new TestServer(webHostBuilder());
+				AddExtra(server);
+				IServiceProvider serviceProvider = server.Host.Services;
+				HttpClient client = server.CreateClient();
+				AddExtra(client);
+
+				return iocContainerFactory(serviceProvider);
+			});
+
+		/// <summary>ASP.NET Core version of setting up IoC and builders to create the IoC context before each test.</summary>
+		/// <param name="webHostBuilder">The ASP.NET Core web host builder from production code, overridden with mocks using .ConfigureTestServices().</param>
+		/// <param name="iocContainerFactory">Builds the IoC container, such as e.g. 'provider => new IocContainer(provider)'.</param>
+		public static void Initialize(Func<IWebHostBuilder> webHostBuilder, Func<IServiceProvider, IIocContainer> iocContainerFactory) =>
+		Initialize(CleanContextMode.ReCreate, webHostBuilder, iocContainerFactory);
+
+		/// <summary>Get the ASP.NET Core client from the TestServer created in <c>Initialize()</c>.</summary>
+		public static HttpClient GetClient(this ContextBuilder _) => Extras[typeof(HttpClient)] as HttpClient;
+
+		private static void AddExtra<T>(T extra) => Extras[typeof(T)] = extra;
+	}
+}
