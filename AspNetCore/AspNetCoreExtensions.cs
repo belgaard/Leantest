100f091f92adc9f2eb029ddcd4e6d3d635411087
Eduardo Semprebon <eduardobr@gmail.com>
2020-03-27 11:58:43 +0100

    Add IHostBuilder support to AspNetCoreContextBuilderFactory

--- AspNetCore/AspNetCoreExtensions.cs
+++ AspNetCore/AspNetCoreExtensions.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using LeanTest.Core.ExecutionHandling;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 
 namespace LeanTest
 {
@@ -33,11 +34,38 @@ namespace LeanTest
 				return iocContainerFactory(serviceProvider);
 			});
 
+		/// <summary>ASP.NET Core version of setting up IoC and builders.</summary>
+		/// <param name="mode">Always use ReCreate to recreate the IoC container before each test.</param>
+		/// <param name="hostBuilder">The ASP.NET Core host builder from production code, overridden with mocks using .ConfigureTestServices().</param>
+		/// <param name="iocContainerFactory">Creates the IoC container, such as e.g. 'provider => new IocContainer(provider)'.</param>
+		/// <remarks>The passed in web host builder is used to create a <c>TestHost</c>.
+		/// A client is created and can be accessed through <c>GetClient()</c>.
+		/// The test host instance is disposed as the first step of initialisation.</remarks>
+		public static void Initialize(CleanContextMode mode, Func<IHostBuilder> hostBuilder, Func<IServiceProvider, IIocContainer> iocContainerFactory) =>
+			ContextBuilderFactory.Initialize(mode, () =>
+			{
+				_testServer?.Dispose();
+				_testServer = null;
+				_client = null;
+
+				_testServer = hostBuilder().Build().GetTestServer();
+				IServiceProvider serviceProvider = _testServer.Host.Services;
+				_client = _testServer.CreateClient();
+
+				return iocContainerFactory(serviceProvider);
+			});
+
 		/// <summary>ASP.NET Core version of setting up IoC and builders to create the IoC context before each test.</summary>
 		/// <param name="webHostBuilder">The ASP.NET Core web host builder from production code, overridden with mocks using .ConfigureTestServices().</param>
 		/// <param name="iocContainerFactory">Builds the IoC container, such as e.g. 'provider => new IocContainer(provider)'.</param>
 		public static void Initialize(Func<IWebHostBuilder> webHostBuilder, Func<IServiceProvider, IIocContainer> iocContainerFactory) =>
 		Initialize(CleanContextMode.ReCreate, webHostBuilder, iocContainerFactory);
+
+		/// <summary>ASP.NET Core version of setting up IoC and builders to create the IoC context before each test.</summary>
+		/// <param name="hostBuilder">The ASP.NET Core host builder from production code, overridden with mocks using .ConfigureTestServices().</param>
+		/// <param name="iocContainerFactory">Builds the IoC container, such as e.g. 'provider => new IocContainer(provider)'.</param>
+		public static void Initialize(Func<IHostBuilder> hostBuilder, Func<IServiceProvider, IIocContainer> iocContainerFactory) =>
+		Initialize(CleanContextMode.ReCreate, hostBuilder, iocContainerFactory);
 
 		/// <summary>Get the ASP.NET Core <c>TestServer</c> created in <c>Initialize()</c>.</summary>
 		public static TestServer GetTestServer(this ContextBuilder _) => _testServer;
