32d92a3eac2d6f1d12c5615f71a9af872f67c69f
Brian Elgaard Bennett <bb@saxobank.com>
2020-08-26 09:47:42 +0200

    Not finished. Introduced IContextBuilder

--- Source/Core/Core/ExecutionHandling/ContextBuilder.cs
+++ Source/Core/Core/ExecutionHandling/ContextBuilder.cs
@@ -5,10 +5,8 @@ using System.Reflection;
 
 namespace LeanTest.Core.ExecutionHandling
 {
-	/// <summary>
-	/// Encapsulates the IoC container and builds the data and execution context for a test, including 'state' and 'mocks'.
-	/// </summary>
-	public class ContextBuilder
+	/// <inheritdoc />
+	public class ContextBuilder : IContextBuilder
 	{
 		private readonly IIocContainer _container;
 		private readonly IBuilder[] _builders;
@@ -23,11 +21,11 @@ namespace LeanTest.Core.ExecutionHandling
 			_builders = builderFactories?.Select(builderFactory => builderFactory(_container, DataStore)).ToArray() ?? throw new ArgumentNullException(nameof(builderFactories));
 		}
 
-		/// <summary>Get an instance of type <c>T</c> from the IoC container.</summary>
+		/// <inheritdoc />
 		public T GetInstance<T>() where T : class => _container.Resolve<T>();
 
-	    /// <summary>Declare data of type <c>T</c> to be stored, then used to fill in builders (e.g. 'mocks' and 'state') during <c>Build</c>.</summary>
-	    public ContextBuilder WithData<T>(T data)
+		/// <inheritdoc />
+		public IContextBuilder WithData<T>(T data)
 	    {
 		    DataStore.WithData(data);
 		    foreach (IBuilder builder in _builders)
@@ -36,9 +34,8 @@ namespace LeanTest.Core.ExecutionHandling
 		    return this;
 	    }
 
-		/// <summary>Pre-declare the intent to handle data of type <c>T</c>. The effect will be to have <c>PreBuild</c>, <c>Build</c> and <c>PostBuild</c> run for builders that
-		/// support data of type <c>T</c>, even for tests which do not declare data of type <c>T</c>.</summary>
-		public ContextBuilder WithData<T>()
+		/// <inheritdoc />
+		public IContextBuilder WithData<T>()
 		{
 			DataStore.WithData<T>();
 			foreach (IBuilder builder in _builders)
@@ -47,8 +44,8 @@ namespace LeanTest.Core.ExecutionHandling
 			return this;
 		}
 
-		/// <summary>Declare an enumeration of data of type <c>T</c> to be stored, then used to fill builders (e.g. 'mocks' and 'state') during <c>Build</c>.</summary>
-		public ContextBuilder WithEnumerableData<T>(IEnumerable<T> ts)
+		/// <inheritdoc />
+		public IContextBuilder WithEnumerableData<T>(IEnumerable<T> ts)
 		{
 			DataStore.WithEnumerable(ts);
 			foreach (IBuilder builder in _builders)
@@ -57,16 +54,16 @@ namespace LeanTest.Core.ExecutionHandling
 			return this;
 		}
 
-		/// <summary>Clear all declared data from the data store.</summary>
-		public ContextBuilder WithClearDataStore()
+		/// <inheritdoc />
+		public IContextBuilder WithClearDataStore()
 		{
 			DataStore.TypedData.Clear();
 
 			return this;
 		}
 
-		/// <summary>Use the declared data to build builders (e.g. 'mocks' and 'state').</summary>
-		public ContextBuilder Build()
+		/// <inheritdoc />
+		public IContextBuilder Build()
 		{
 			try
 			{
