0aca20943900756e3a99cd51b25d18945bd276b6
Brian Elgaard <belgaard@users.noreply.github.com>
2020-09-09 15:33:36 +0200

    Updated dependencies (#87)
    Co-authored-by: Brian Elgaard Bennett <bb@saxobank.com>

--- Source/MSTest/TestTagAttribute.cs
+++ Source/MSTest/TestTagAttribute.cs
@@ -6,11 +6,11 @@ namespace LeanTest.MSTest
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class TestTagAttribute : Attribute
     {
-	    /// <summary>A prefix put in a .trx-file before each tag value.</summary>
+        /// <summary>A prefix put in a .trx-file before each tag value.</summary>
         public const string Prefix = @"TestTag = ###---";
-	    /// <summary>A postfix put in a .trx-file after each tag value.</summary>
+        /// <summary>A postfix put in a .trx-file after each tag value.</summary>
         public const string Postfix = @"---###";
-	    /// <summary>Put in a .trx-file for each 'not implemented' tag value.</summary>
+        /// <summary>Put in a .trx-file for each 'not implemented' tag value.</summary>
         public const string NotImplemented = @"LeanTest.NotImplemented";
 
         /// <inheritdoc />
