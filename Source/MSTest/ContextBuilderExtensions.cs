67f8e053d66f47864e7b580d716a90d58bb54d26
Brian Elgaard Bennett <bb@saxobank.com>
2020-09-10 17:24:25 +0200

    untabify

--- Source/MSTest/ContextBuilderExtensions.cs
+++ Source/MSTest/ContextBuilderExtensions.cs
@@ -7,27 +7,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeanTest.MSTest
 {
-	/// <summary>Adds support for adding information on tests to test run results.</summary>
+    /// <summary>Adds support for adding information on tests to test run results.</summary>
     public static class ContextBuilderExtensions
     {
         /// <summary>Registers an intent to use the <c>TestScenarioId</c> attribute on test methods.</summary>
         /// <remarks>This causes LeanTest scenario IDs to be written to the test log (.trx-file).</remarks>
         public static ContextBuilder RegisterScenarioId(this ContextBuilder theContextBuilder, TestContext testContext, Assembly assembly = null) => 
-	        theContextBuilder.RegisterScenarioId(testContext.TestName, assembly ?? Assembly.GetCallingAssembly(), typeof(TestScenarioIdAttribute));
+            theContextBuilder.RegisterScenarioId(testContext.TestName, assembly ?? Assembly.GetCallingAssembly(), typeof(TestScenarioIdAttribute));
 
         /// <summary>Registers an intent to use the <c>TestTag</c> attribute on test methods.</summary>
         /// <remarks>This causes LeanTest tags to be written to the test log (.trx-file).</remarks>
         public static ContextBuilder RegisterTags(this ContextBuilder theContextBuilder, TestContext testContext, Assembly assembly = null) => 
-	        theContextBuilder.RegisterTags(testContext.TestName, assembly ?? Assembly.GetCallingAssembly(), typeof(TestTagAttribute));
+            theContextBuilder.RegisterTags(testContext.TestName, assembly ?? Assembly.GetCallingAssembly(), typeof(TestTagAttribute));
 
         /// <summary>Registers an intent to use the <c>Description</c> attribute on test methods.</summary>
         /// <remarks>This causes MsTest descriptions to be written to the test log (.trx-file).</remarks>
         public static ContextBuilder RegisterDescription(this ContextBuilder theContextBuilder, TestContext testContext, Assembly assembly = null)
         {
-	        theContextBuilder.RegisterDescription(testContext.TestName, assembly ?? Assembly.GetCallingAssembly());
+            theContextBuilder.RegisterDescription(testContext.TestName, assembly ?? Assembly.GetCallingAssembly());
 
-			// The following is for backwards compatibility with the MsTest Description attribute. We prefer to use the new LeanTest TestDescription
-			// attribute:
+            // The following is for backwards compatibility with the MsTest Description attribute. We prefer to use the new LeanTest TestDescription
+            // attribute:
             assembly ??= Assembly.GetCallingAssembly();
             MethodInfo[] methods = assembly.GetTypes()
                 .SelectMany(t => t.GetMethods())
