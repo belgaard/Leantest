c4da06d0a83564464780968a150eba6ca2564545
Brian Elgaard <belgaard@users.noreply.github.com>
2020-09-10 18:17:31 +0200

    The Xunit adaption to MultiAssertForTException (#92)
    Co-authored-by: Brian Elgaard Bennett <bb@saxobank.com>

---
+++ Source/xUnit/MultiAssert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LeanTest.Core.ExecutionHandling;
+using Xunit.Sdk;
+
+namespace LeanTest.Xunit
+{
+	/// <summary>The Xunit adaption to <c>MultiAssertForTException</c>.</summary>
+	public static class MultiAssert
+	{
+		/// <summary>Executes all actions regardless of failed assertions (a.k.a. <c>AssertFailedException</c> thrown). If any assertions fail, a single 
+		/// assertion failure with aggregated assertion message texts is generated.</summary>
+		public static void Aggregate(params Action[] actions)
+		{
+			try
+			{
+				MultiAssertForTException.Aggregate<XunitException>(actions);
+			}
+			// Turn aggregated messages into a proper MS Test assert failed exception - but let other exceptions, including 'inconclusive' fall through:
+			catch (AggregatedMessagesException e)
+			{
+				throw new XunitException(e.Message);
+			}
+		}
+	}
+}
