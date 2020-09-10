bd0cb84fad18fdbedd6768374750e3a526b17eee
Brian Elgaard Bennett <bb@saxobank.com>
2020-09-10 17:15:57 +0200

    Added generic functionality, i.e. for no test framework in particular, for attributes

---
+++ Source/Core/Attribute/TestTagAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeanTest.Attribute
+{
+    /// <summary>The test tag attribute.</summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class TestTagAttribute : System.Attribute, IAttributeValue
+    {
+        /// <summary>A prefix put in a .trx-file before each tag value.</summary>
+        public const string Prefix = @"TestTag = ###---";
+        /// <summary>A postfix put in a .trx-file after each tag value.</summary>
+        public const string Postfix = @"---###";
+        /// <summary>Put in a .trx-file for each 'not implemented' tag value.</summary>
+        public const string NotImplemented = @"LeanTest.NotImplemented";
+
+        /// <inheritdoc />
+        public TestTagAttribute(string value) => Value = value;
+        /// <summary>The value.</summary>
+        public string Value { get; }
+    }
+}
