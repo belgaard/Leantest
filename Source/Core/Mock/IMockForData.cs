eedd2851e9bcec7f461caec97975e93534774072
Brian Elgaard Bennett <bb@saxobank.com>
2021-06-09 12:38:29 +0200

    Interface default implementations. No default for WithData - you do need to at least receive initial context (IC)

--- Source/Core/Mock/IMockForData.cs
+++ Source/Core/Mock/IMockForData.cs
@@ -8,10 +8,10 @@ namespace LeanTest.Mock
         /// <summary>Declare data of type <c>T</c>.</summary>
         void WithData(T data);
         /// <summary>Called before build only once for the instance, allows you to prepare to populate state.</summary>
-        void PreBuild();
+        void PreBuild() {}
         /// <summary>Use the declared data to populate state, called after all data of type <c>type</c> has been put to the instance with <c>WithData</c>.</summary>
-        void Build(Type type);
+        void Build(Type type) {}
         /// <summary>Called after build, only once for the instance.</summary>
-        void PostBuild();
+        void PostBuild() {}
     }
 }
