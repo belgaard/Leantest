66f4c4374f4bb6b08719d8fc8260046dd468290d
Brian Elgaard <belgaard@users.noreply.github.com>
2021-07-08 13:58:01 +0200

    Changed the example to use the new factory-free way of creating a context builder. Also updated dependencies. (#103)
    Co-authored-by: Brian Elgaard Bennett <bb@saxobank.com>

---
+++ Source/Examples.L0Tests/TestSetup/MyContextBuilderFactory.cs
@@ -0,0 +1,15 @@
+﻿using Examples.L0Tests.Application;
+using Examples.L0Tests.TestSetup.IoC;
+using LeanTest.Core.ExecutionHandling;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Examples.L0Tests.TestSetup
+{
+	/// <summary>Does the setup which must must be done consistently across all tests in the assembly.</summary>
+	public class MyContextBuilderFactory
+	{
+		public ContextBuilder ContextBuilder {get;}
+		public MyContextBuilderFactory() => 
+			ContextBuilder = new ContextBuilder(new IocContainer(L0CompositionRootForTest.Initialize(CompositionRoot.Initialize(new ServiceCollection()))));
+	}
+}
