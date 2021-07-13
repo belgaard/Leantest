b0d6d3796cc6d3717862bd76895b838a051afeaf
Brian Elgaard Bennett <bb@saxobank.com>
2021-07-13 12:05:17 +0200

    Introduced the new DI.DotNetCore package

---
+++ Source/DI.DotNetCore/IocContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using LeanTest.Core.ExecutionHandling;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI.DotNetCore
+{
+	/// <inheritdoc/>
+	public class IocContainer : IIocContainer
+	{
+		private readonly IServiceProvider _serviceProvider;
+
+		/// <summary>The ctor requires a service provider, you can generate it from a service collection using <c>.BuildServiceProvider()</c></summary>
+        public IocContainer(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+		/// <inheritdoc/>
+		/// <remarks>Gets a required service from the service provider.</remarks>
+		public T Resolve<T>() where T : class => _serviceProvider.GetRequiredService<T>();
+		/// <inheritdoc/>
+		public T TryResolve<T>() where T : class => _serviceProvider.GetService<T>();
+		/// <inheritdoc/>
+		/// <remarks>The full set of mock-for-data and state handlers is returned.</remarks>
+		public IEnumerable<T> TryResolveAll<T>() where T : class => _serviceProvider.GetServices<T>();
+	}
+}
