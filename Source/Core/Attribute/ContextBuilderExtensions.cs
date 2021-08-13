613b153758e66179c370f848a1fbd9f3044d1266
Brian Elgaard Bennett <bb@saxobank.com>
2021-08-13 13:36:25 +0200

    Introduced IStdOut

--- Source/Core/Attribute/ContextBuilderExtensions.cs
+++ Source/Core/Attribute/ContextBuilderExtensions.cs
@@ -5,70 +5,71 @@ using LeanTest.Core.ExecutionHandling;
 
 namespace LeanTest.Attribute
 {
-    /// <summary>Adds support for adding information on tests to test run results.</summary>
-    public static class ContextBuilderExtensions
+	/// <summary>Adds support for adding information on tests to test run results.</summary>
+	public static class ContextBuilderExtensions
     {
-        private static class EmptyArray<T>
-        {
-            internal static readonly T[] Value = new T[0];
-        }
+	    private static readonly IStdOut DefaultStdOut = new StdOutConsole();
+
         /// <summary>Registers an intent to use the <c>TestScenarioId</c> attribute on test methods.</summary>
         /// <remarks>This causes LeanTest scenario IDs to be written to the test log (.trx-file).</remarks>
-        public static ContextBuilder RegisterScenarioId(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null)
+        public static ContextBuilder RegisterScenarioId(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null, IStdOut stdOut = null)
         {
             assembly ??= Assembly.GetCallingAssembly();
+            stdOut ??= DefaultStdOut;
             Type[] types = BothTypes(extraType, typeof(TestScenarioIdAttribute));
             MethodInfo[] allMethodsForTest = GetMethodsForTest(testName, assembly);
 
             foreach (Type type in types)
                 foreach (MethodInfo methodInfo in allMethodsForTest)
                     foreach (IAttributeValue attribute in methodInfo.GetCustomAttributes(type, false))
-                        Console.WriteLine($@"{TestScenarioIdAttribute.Prefix}{attribute.Value}{TestScenarioIdAttribute.Postfix}");
+                        stdOut.WriteLine($@"{TestScenarioIdAttribute.Prefix}{attribute.Value}{TestScenarioIdAttribute.Postfix}");
 
             return theContextBuilder;
         }
 
         /// <summary>Registers an intent to use the <c>TestTag</c> attribute on test methods.</summary>
         /// <remarks>This causes LeanTest tags to be written to the test log (.trx-file).</remarks>
-        public static ContextBuilder RegisterTags(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null)
+        public static ContextBuilder RegisterTags(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null, IStdOut stdOut = null)
         {
             assembly ??= Assembly.GetCallingAssembly();
+            stdOut ??= DefaultStdOut;
             Type[] types = BothTypes(extraType, typeof(TestTagAttribute));
             MethodInfo[] allMethodsForTest = GetMethodsForTest(testName, assembly);
 
             foreach (Type type in types)
                 foreach (MethodInfo methodInfo in allMethodsForTest)
                     foreach (IAttributeValue attribute in methodInfo.GetCustomAttributes(type, false))
-                        Console.WriteLine($@"{TestTagAttribute.Prefix}{attribute.Value}{TestTagAttribute.Postfix}");
+                        stdOut.WriteLine($@"{TestTagAttribute.Prefix}{attribute.Value}{TestTagAttribute.Postfix}");
 
             return theContextBuilder;
         }
 
         /// <summary>Registers an intent to use the <c>Description</c> attribute on test methods.</summary>
         /// <remarks>This causes MsTest descriptions to be written to the test log (.trx-file).</remarks>
-        public static ContextBuilder RegisterDescription(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null)
+        public static ContextBuilder RegisterDescription(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, Type extraType = null, IStdOut stdOut = null)
         {
             assembly ??= Assembly.GetCallingAssembly();
+            stdOut ??= DefaultStdOut;
             Type[] types = BothTypes(extraType, typeof(TestDescriptionAttribute));
             MethodInfo[] allMethodsForTest = GetMethodsForTest(testName, assembly);
 
             foreach (Type type in types)
                 foreach (MethodInfo methodInfo in allMethodsForTest)
                     foreach (IAttributeValue attribute in methodInfo.GetCustomAttributes(type, false))
-                        Console.WriteLine($@"{TestDescriptionAttribute.Prefix}{attribute.Value}{TestDescriptionAttribute.Postfix}");
+                        stdOut.WriteLine($@"{TestDescriptionAttribute.Prefix}{attribute.Value}{TestDescriptionAttribute.Postfix}");
 
             return theContextBuilder;
         }
 
         /// <summary>Registers an intent to use the LeanTest attribute on test methods.</summary>
         /// <remarks>This causes LeanTest scenario IDs and tags as well as MsTest descriptions to be written to the test log (.trx-file).</remarks>
-        public static ContextBuilder RegisterAttributes(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null)
+        public static ContextBuilder RegisterAttributes(this ContextBuilder theContextBuilder, string testName, Assembly assembly = null, IStdOut stdOut = null)
         {
             assembly ??= Assembly.GetCallingAssembly();
             return theContextBuilder
-                .RegisterDescription(testName, assembly)
-                .RegisterScenarioId(testName, assembly)
-                .RegisterTags(testName, assembly);
+                .RegisterDescription(testName, assembly, null, stdOut)
+                .RegisterScenarioId(testName, assembly, null, stdOut)
+                .RegisterTags(testName, assembly, null, stdOut);
         }
  
         private static Type[] BothTypes(Type extraType, Type thisType)
@@ -84,6 +85,10 @@ namespace LeanTest.Attribute
             return assembly.GetTypes()
                 .SelectMany(t => t.GetMethods())
                 .Where(m => m.Name == testName).ToArray();
+        }
+        private class StdOutConsole : IStdOut
+        {
+	        public void WriteLine(string value) => Console.WriteLine(value);
         }
     }
 }
---
+++ Source/Core/Attribute/IStdOut.cs
@@ -0,0 +1,9 @@
+﻿namespace LeanTest.Attribute
+{
+	/// <summary>Used to send output to std out (which will differ across test frameworks).</summary>
+	public interface IStdOut
+	{
+		/// <summary>Write a line to stdout.</summary>
+		void WriteLine(string value);
+	}
+}
